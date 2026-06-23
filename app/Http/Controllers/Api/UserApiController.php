ce390d0840a7bc29fce9aed43caf93882b0091e5
lkiop <lkiop33@gmail.com>
2026-06-23 17:14:00 +0900

    Return all user fields and load department relationship in /api/users endpoint

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -9,16 +9,34 @@ use Illuminate\Http\JsonResponse;
 class UserApiController extends Controller
 {
     /**
-     * Display a listing of all users names.
+     * Display a listing of all users with full information.
      */
     public function index(): JsonResponse
     {
-        $users = User::select('id', 'name', 'nickname')->get()->map(function ($user) {
+        $users = User::with('departmentRel')->get()->map(function ($user) {
             return [
                 'id' => $user->id,
                 'name' => $user->name,
                 'nickname' => $user->nickname,
                 'display_name' => $user->display_name,
+                'email' => $user->email,
+                'rank_type' => $user->rank_type,
+                'department_id' => $user->department_id,
+                'department_name' => $user->departmentRel?->name ?? $user->department ?? 'N/A',
+                'joined_at' => $user->joined_at ? $user->joined_at->format('Y-m-d') : null,
+                'last_login_at' => $user->last_login_at ? $user->last_login_at->format('Y-m-d H:i:s') : null,
+                'birth_date' => $user->birth_date,
+                'location' => $user->location,
+                'part' => $user->part,
+                'account_id' => $user->account_id,
+                'last_access' => $user->last_access,
+                'level' => $user->level,
+                'phone' => $user->phone,
+                'interphone' => $user->interphone,
+                'photo' => $user->photo,
+                'join_date' => $user->join_date,
+                'created_at' => $user->created_at ? $user->created_at->format('Y-m-d H:i:s') : null,
+                'updated_at' => $user->updated_at ? $user->updated_at->format('Y-m-d H:i:s') : null,
             ];
         });
 
